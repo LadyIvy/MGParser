cc13c02b61ff9aaa6f2a5684684ef1e5443e8f3e
Dawid Pogorzelski <dawid.pogorzelski@mybushido.com>
2011-03-09 22:20:24 +0100

    eventmachine and snmp set up

--- MGParser.rb
+++ MGParser.rb
@@ -13,50 +13,57 @@ require 'snmp_gw'
 
 $options = Option.parse(ARGV)
 
-#We initialize eventmachine
-EventMachine::run do
-  		host = UDPSocket.open {|s| s.connect($options.list[:ipaddr]); s.addr.last }
-  		port = $options.list[:port]
-  		EventMachine::open_datagram_socket host, port, Parser
-  		puts "Listening on {host}:{$options.list[:port]}"
-	end
-
-class Parser
-  def initialize(file)
-    if FileTest::exist?(file)
-      file_lines = IO.readlines(file)
-      analyzer(file_lines)
-    else
-      puts "File non trovato"
-    end
-  end
-  
-  def analyzer(file_lines)
-    file_lines.each do |line|    
-      if 
-        line =~ /Calling Party.*/
-        caller = line.scan(/Calling Party.*'\d.*\d/).first.gsub(/Calling Party.* '/,"")
-        puts "Chiamata da parte di: {caller}"
-      elsif 
-        line =~ /Called Party.*/
-        called = line.scan(/Called Party.*'\d.*\d/).first.gsub(/Called Party.* '/,"")
-        puts "Verso: {called}"
-      elsif
-        line =~ /SEND Setup/
-        puts "Setup ISDN inviato"
-      elsif
-        line =~ /Received ISDN message "Progress Indication"/
-        puts "Ricevuto \"Call Progress\" , l'operatore sta gestendo la chiamata"
-      elsif
-        line =~ /Received ISDN message "Disconnect Indication"/
-        puts "L\'operatore ha richiesto il riaggancio di chiamata"
-      elsif
-        line =~ /Cause: Normal, unspecified [(]31[)]/
-        puts "Il numero chiamato e\' occupato!"
-      end
-    end
+module SyslogServer
+include EM::Protocols::LineText2
+  def receive_line(data)
+    puts "#{data}"
   end
 end
 
-parser = Parser.new(ARGV.first)
-parser
+#We initialize eventmachine
+EventMachine::run do
+  host = UDPSocket.open {|s| s.connect($options.list[:ipaddr],30000); s.addr.last }
+	port = $options.list[:port]
+	snmpgw = SnmpGw.new($options.list[:port],$options.list[:ipaddr])
+	EventMachine::start_server host, port, SyslogServer
+end
+	
+#class Parser
+#  def initialize(file)
+#    if FileTest::exist?(file)
+#      file_lines = IO.readlines(file)
+#      analyzer(file_lines)
+#    else
+#      puts "File non trovato"
+#    end
+#  end
+#  
+#  def analyzer(file_lines)
+#    file_lines.each do |line|    
+#      if 
+#        line =~ /Calling Party.*/
+#        caller = line.scan(/Calling Party.*'\d.*\d/).first.gsub(/Calling Party.* '/,"")
+#        puts "Chiamata da parte di: {caller}"
+#      elsif 
+#        line =~ /Called Party.*/
+#        called = line.scan(/Called Party.*'\d.*\d/).first.gsub(/Called Party.* '/,"")
+#        puts "Verso: {called}"
+#      elsif
+#        line =~ /SEND Setup/
+#        puts "Setup ISDN inviato"
+#      elsif
+#        line =~ /Received ISDN message "Progress Indication"/
+#        puts "Ricevuto \"Call Progress\" , l'operatore sta gestendo la chiamata"
+#      elsif
+#        line =~ /Received ISDN message "Disconnect Indication"/
+#        puts "L\'operatore ha richiesto il riaggancio di chiamata"
+#      elsif
+#        line =~ /Cause: Normal, unspecified [(]31[)]/
+#        puts "Il numero chiamato e\' occupato!"
+#      end
+#    end
+#  end
+#end
+
+#parser = Parser.new(ARGV.first)
+#parser
