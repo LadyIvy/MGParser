20f4a55a02c69e368ac613c54c1ff26c4a7a3d8c
Dawid Pogorzelski <dawid.pogorzelski@mybushido.com>
2011-04-17 00:45:04 +0200

    Several changes in parsing scheme

--- MGParser.rb
+++ MGParser.rb
@@ -24,44 +24,63 @@ module SyslogServer
     trace = data.gsub(/<191>syslog: /,"")
     trace.each do |line|
     #TODO need to analyze callid of each line in order to print it at starting of each line
-      if 
-          line =~ /CreateCall.*\d/
-          @sourceID, @destID = line.scan(/CreateCall.*\d/).first.gsub(/CreateCall.*create call /,"").split("-")
-          #calls_hash['call_with_id' + @sourceID.to_s]=Call.new(@sourceID, @destID)
-          #call = Call.new(@sourceID, @destID)
-          
-      elsif
-          line =~ /CallRouter \[.*\] Src=\[CID:\d{1,}/
-          src = line.scan(/CallRouter \[.*\] Src=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Src=\[CID:/,"")
-          if src == @sourceID
-            caller = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
-            puts "call_#{@sourceID} - Caller: #{caller}"
-          end
-      elsif 
-          line =~ /CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/
-          dst = line.scan(/CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Dst2\/2=\[CID:/,"")
-          if dst == (@destID += 1)
-            clled = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
-            puts "call_#{@sourceID} - Called: #{called}"
-          end
-      elsif
-         line =~ /CallManager \[.*\] C\d{1,} - Send CallSetupA/
-         id = line.scan(/CallManager \[.*\] C\d{1,}/).first.gsub(/CallManager \[.*\] C/,"")
-         if id == (@destID += 1)
-           puts "call_#{@sourceID} - ISDN setup sent"
-         end
-      elsif
-         line =~ /Received ISDN message "Progress Indication"/
-         puts "\"Call Progress\" received, call is proceeding"
-      elsif
-         line =~ /Received ISDN message "Disconnect Indication"/
-         puts "Operator requested hangup"
-      elsif
-         line =~ /Cause: Normal, unspecified [(]31[)]/
-         puts "Called numer is busy!"
-      end
+    if line =~ /CreateCall.*\d/
+        @sourceID, @destID = line.scan(/CreateCall.*\d/).first.gsub(/CreateCall.*create call /,"").split("-")
+        @destID = @destID.to_i
+
+    elsif line =~ /CallRouter \[.*\] Src=\[CID:\d{1,}.*speech,date/
+        src = line.scan(/CallRouter \[.*\] Src=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Src=\[CID:/,"")
+        if src == @sourceID
+          caller = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
+          puts "Call ID: #{@sourceID} - Caller: #{caller}"
+        end
+        
+    elsif line =~ /CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/
+        dst = line.scan(/CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Dst2\/2=\[CID:/,"")
+        if dst.to_i == (@destID += 1)
+          called = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
+          puts "Call ID: #{@sourceID} - Called: #{called}"
+        end
+        
+    elsif line =~ /CallManager \[.*\] C\d{1,} - Send CallSetupA/
+        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+        if id.to_i == (@destID)
+          puts "Call ID: #{@sourceID} - ISDN setup sent"
+        end
+        
+    elsif line =~ /"Proceeding Indication" .* for state CallSetup./
+        @progress_indication = true
+        
+    elsif line =~ /CallManager \[.*\] C\d{1,} - CallProgressA\(2\)/
+        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+        if id.to_i == (@destID)
+          if @progress_indication == true
+            puts "Call ID: #{@sourceID} - \"Proceeding indication\" received from operator" 
+            @progress_indication = false
+          else
+            puts "Call ID: #{@sourceID} - \"Call Progress\" received from operator"
+          end 
+        end
+        
+    elsif line =~ /CallManager \[.*\] C\d{1,} - CallProgressA\(1\)/
+        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+        if id.to_i == (@destID)
+          puts "Call ID: #{@sourceID} - Destination number is ringing" 
+        end
+        
+    elsif line =~ /CallManager \[.*\] C\d{1,} - CallConnectA/
+        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+        if id.to_i == (@destID)
+          puts "Call ID: #{@sourceID} - Call has been answered!" 
+        end
+        
+    elsif line =~ /Received ISDN message "Disconnect Indication"/
+        puts "Call ID: #{@sourceID} - Operator requested hangup"
+
+    elsif line =~ /Cause: Normal, unspecified [(]31[)]/
+        puts "Call ID: #{@sourceID} - Called numer is busy!"
+    end
   end
-    #puts trace
     
   end
 end
