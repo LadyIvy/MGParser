23aa9cb412707b50b6beabce0ebb41cfc910ae7d
Dawid Pogorzelski <dawid.pogorzelski@mybushido.com>
2011-04-18 23:26:18 +0200

    Added some exception handling

--- lib/snmp_gw.rb
+++ lib/snmp_gw.rb
@@ -9,18 +9,18 @@ class SnmpGw
 attr_accessor :port, :ipaddr, :snmp
 
 def initialize(port, ipaddr, snmp)
-  #begin
-    #raise RuntimeError, "Host unreachable!"
-    manager = Manager.new(:Host => ipaddr,:Port => snmp) 
+  begin
+    manager = Manager.new(:Host => ipaddr,:Port => snmp)     
     local_ip = UDPSocket.open {|s| s.connect($options.list[:ipaddr],30001); s.addr.last }
     diagnosticTracesEnable = VarBind.new("1.3.6.1.4.1.4935.1000.100.200.100.1100.1.10000.100.0", SNMP::Integer.new("1"))
     syslogRemoteHost = VarBind.new("1.3.6.1.4.1.4935.1000.100.200.100.1100.1.100.100.0", OctetString.new("#{local_ip}:#{$options.list[:port]}"))
     manager.set(diagnosticTracesEnable)
     manager.set(syslogRemoteHost)
     manager.close
-#rescue Exception => e
-    #puts e.message
-    #abort""
-#end
+  rescue Errno::EHOSTUNREACH, Errno::EHOSTDOWN
+    puts "Host Unreachable!"
+    abort""
+  end
+
 end
 end
