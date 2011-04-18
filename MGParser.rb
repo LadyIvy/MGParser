9c103ff0dca07dc3eba23e36fb11e6cd9d0b00e6
Dawid Pogorzelski <dawid.pogorzelski@mybushido.com>
2011-04-18 22:54:42 +0200

    Simplified parsing system

--- MGParser.rb
+++ MGParser.rb
@@ -26,81 +26,71 @@ module SyslogServer
     @callprogr_counter = 0
     trace.each do |line|
 
-    if line =~ /CreateCall.*\d/
-        @sourceID, @destID = line.scan(/CreateCall.*\d/).first.gsub(/CreateCall.*create call /,"").split("-")
-        @destID = @destID.to_i
+      if line =~ /CallRouter \[.*\] Src=\[CID:\d{1,}.*speech,date/
+         src = line.scan(/CallRouter \[.*\] Src=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Src=\[CID:/,"")
+             caller = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
+             puts "Call ID: #{src.to_i+2} - Caller: #{caller}"
 
-    elsif line =~ /CallRouter \[.*\] Src=\[CID:\d{1,}.*speech,date/
-        src = line.scan(/CallRouter \[.*\] Src=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Src=\[CID:/,"")
-        if src == @sourceID
-          caller = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
-          puts "Call ID: #{@sourceID} - Caller: #{caller}"
-        end
-        
-    elsif line =~ /CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/
-        dst = line.scan(/CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Dst2\/2=\[CID:/,"")
-        if dst.to_i == (@destID += 1)
-          called = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
-          puts "Call ID: #{@sourceID} - Called: #{called}"
-        end
-        
-    elsif line =~ /CallManager \[.*\] C\d{1,} - Send CallSetupA/
-        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
-        if id.to_i == (@destID)
-          puts "Call ID: #{@sourceID} - ==> ISDN setup sent"
-        end
-        
-    elsif line =~ /"Proceeding Indication" .* for state CallSetup./
-        @progress_indication = true
-        
-   elsif line =~ /CallManager \[.*\] C\d{1,} - CallProgressA\(2\)/
-        @callprogr_counter += 1
-        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
-        if id.to_i == (@destID)
-          if @progress_indication == true
-            puts "Call ID: #{@sourceID} - <== \"Proceeding indication\" received from operator" 
-            @progress_indication = false
-          else
-            if @callprogr_counter >=3 
-              puts "Call ID: #{@sourceID} - <== The call will be probably answered by operator\'s voicemail or is being forwarded"
-              @callprogr_counter = 0
-            else
-                puts "Call ID: #{@sourceID} - <== \"Call Progress\" received from operator"
-            end
-          end 
-        end
-        
-    elsif line =~ /CallManager \[.*\] C\d{1,} - CallProgressA\(1\)/
-        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
-        if id.to_i == (@destID)
-          puts "Call ID: #{@sourceID} - <== Destination number is ringing" 
-        end
-        
-    elsif line =~ /CallManager \[.*\] C\d{1,} - CallConnectA/
-        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
-        if id.to_i == (@destID)
-          puts "Call ID: #{@sourceID} - <== Call has been answered!" 
-        end
-        
-    elsif line =~ /CallManager \[.*\] C\d{1,} - CallMessageA\(2\)/
-        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
-        if id.to_i == (@destID)
-          @isdn_inbound_disconnect = true
-        end
-          
-    elsif line =~ /CallManager \[.*\] C\d{1,} - Send CallReleaseA\(\d{1,}\)/
-        id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
-        cause = line.scan(/Send CallReleaseA\(\d{1,}/).first.gsub(/Send CallReleaseA\(/,"")
-        if id.to_i == (@destID) 
-          if @isdn_inbound_disconnect == true
-            puts "Call ID: #{@sourceID} - <== Operator requested hangup with cause: \"#{$causes[cause]}\""
-            @isdn_inbound_disconnect = false
-          else
-            puts "Call ID: #{@sourceID} - ==> Gateway sent hangup with cause: \"#{$causes[cause]}\""
-          end
-        end
 
-    end
+
+       elsif line =~ /CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/
+         dst = line.scan(/CallRouter \[.*\] Dst2\/2=\[CID:\d{1,}/).first.gsub(/CallRouter \[.*\] Dst2\/2=\[CID:/,"")
+             called = line.scan(/e164=\d{1,}/).first.gsub(/e164=/,"")
+             puts "Call ID: #{dst} - Called: #{called}"
+
+
+       elsif line =~ /CallManager \[.*\] C\d{1,} - Send CallSetupA/
+           id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+             puts "Call ID: #{id} - ==> ISDN setup sent"
+
+
+       elsif line =~ /"Proceeding Indication" .* for state CallSetup./
+           @progress_indication = true
+
+       elsif line =~ /CallManager \[.*\] C\d{1,} - CallProgressA\(2\)/
+           @callprogr_counter += 1
+           id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+
+             if @progress_indication == true
+               puts "Call ID: #{id} - <== \"Proceeding indication\" received from operator" 
+               @progress_indication = false
+             else
+               if @callprogr_counter >= 3
+                 puts "Call ID: #{id} - <== The call will be probably answered by operator\'s voicemail or is being forwarded"
+                 @callprogr_counter = 0
+               else
+                 puts "Call ID: #{id} - <== \"Call Progress\" received from operator"
+               end
+             end 
+
+
+       elsif line =~ /CallManager \[.*\] C\d{1,} - CallProgressA\(1\)/
+           id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+             puts "Call ID: #{id} - <== Destination number is ringing" 
+
+
+       elsif line =~ /CallManager \[.*\] C\d{1,} - CallConnectA/
+           id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+             puts "Call ID: #{id} - <== Call has been answered!" 
+
+
+       elsif line =~ /CallManager \[.*\] C\d{1,} - CallMessageA\(2\)/
+           id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+             @isdn_inbound_disconnect = true
+
+
+       elsif line =~ /CallManager \[.*\] C\d{1,} - Send CallReleaseA\(\d{1,}\)/
+           id = line.scan(/C\d{1,}/).first.gsub(/C/,"")
+           cause = line.scan(/Send CallReleaseA\(\d{1,}/).first.gsub(/Send CallReleaseA\(/,"")
+
+             if @isdn_inbound_disconnect == true
+               puts "Call ID: #{id} - <== Operator requested hangup with cause: \"#{$causes[cause]}\""
+               @isdn_inbound_disconnect = false
+             else
+               puts "Call ID: #{id} - ==> Gateway sent hangup with cause: \"#{$causes[cause]}\""
+             end
+
+       end
   end
     
   end
