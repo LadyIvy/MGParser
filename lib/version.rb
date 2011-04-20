332c16531d784035f3316238916bb106d4ab777a
Dawid Pogorzelski <dawid.pogorzelski@mybushido.com>
2011-04-20 23:17:38 +0200

    Version bump

--- lib/version.rb
+++ lib/version.rb
@@ -2,7 +2,7 @@ module Mgparser
 module VERSION
     MAJOR = 0 unless defined? MAJOR
     MINOR = 1 unless defined? MINOR
-    TINY  = 2 unless defined? TINY
+    TINY  = 3 unless defined? TINY
     
     STRING = [MAJOR, MINOR, TINY].join('.') unless defined? STRING
   end
