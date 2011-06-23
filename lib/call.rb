d9b3ca398838aacdb5a8f91e3d4971d84f7e4c68
Dawid Pogorzelski <dawid.pogorzelski@mybushido.com>
2011-06-23 20:50:43 +0200

    added some description

--- lib/call.rb
+++ lib/call.rb
@@ -1,6 +1,8 @@
 class Call
 attr_accessor :data
 
+  # "analyze" will choose how to process syslog with "parse method" 
+   
   def analyze(data,is_file)
      
     if is_file == true
@@ -13,9 +15,20 @@ attr_accessor :data
 
   end
 
+  # parse method will parse each line of incoming syslog and print filtered content
+  
   def parse(line)
+    
+      # we check for call inizialization message and set:
+      # @id = call id number (this will help to understand which call are the messages related to)
+      # @direction = is needed to determine if the call is inbound or outbound
+      # @content = is the content of the DSS1 message
+      
       if line =~ /IsdnStackL3Msg.*Call \d{1,}-(In|Out)bound/
         @id, @direction, @content = line.scan(/IsdnStackL3Msg.*Call (\d{1,})-.*(RECV|SEND) (\w{1,}.*) \(/).flatten!
+        
+        #we print message based on @direction (if the message content is "Notify" don't do anything
+        # and wait for "Notification Indicator" message instead because it contains notification description )
         
         if @direction == "SEND" and @content != "Disconnect" and @content != "Release"
           puts "Call #{@id} - ==> Sent \"#{@content}\""
@@ -24,6 +37,8 @@ attr_accessor :data
         elsif @direction == "RECV" and @content != "Disconnect" and @content != "Release"
           puts "Call #{@id} - <== Received \"#{@content}\""
         end
+      
+      # here we check for caller and called numbers 
         
       elsif line =~ /IsdnStackL3Msg.*IE (Called|Calling) Party Number/
         origin, number = line.scan(/IsdnStackL3Msg.*IE (Called|Calling) Party Number.*'(\d{1,})'/).flatten!
@@ -32,10 +47,14 @@ attr_accessor :data
         elsif origin == "Calling"
           puts "Call #{@id} - Calling number: #{number}"
         end
+      
+      # we print notification messages description
         
       elsif line =~ /IsdnStackL3Msg.*IE Notification Indicator/
         notification = line.scan(/IsdnStackL3Msg.*IE Notification Indicator.*Description: (.*) \(/).flatten!.first
         puts "Call #{@id} - <== Received Notify: \"#{notification}\" (if you called a mobile number it could be the VoiceMail)"
+      
+      # it will print call disconnection cause
       
       elsif line =~ /IsdnStackL3Msg.*IE Cause/
           cause = line.scan(/IsdnStackL3Msg.*IE Cause.* Cause.*\((.*)\)/).flatten!.first
